687d2b09caab54e46fcec866487e9d795226f4e1
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2026-02-25 21:14:41 +0100

    Enhance ping script to suppress output and add network scanning functionality

--- networking/port-scanner/ping.py
+++ networking/port-scanner/ping.py
@@ -1,9 +1,16 @@
 import os
+import ipaddress
 def ping(host):
-    response = os.system("ping -c 1 -W 1 " + host)
+    response = os.system("ping -c 1 -W 1 " + host + " > /dev/null 2>&1")
     if response == 0:
         return True
     else:
         return False
 
-ping("google.com")
+network = "8.8.8.0/28"
+ipaddresss = ipaddress.ip_network(network)
+for ip in ipaddresss.hosts():
+    if ping(str(ip)):
+        print(f"{ip} is alive")
+    else:
+        print(f"{ip} is unreachable")
